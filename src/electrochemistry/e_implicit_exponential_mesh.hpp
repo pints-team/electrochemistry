92e1ae8da1656592eb787bfb054cbf532724903e
Martin Robinson <martinjrobins@gmail.com>
2018-04-19 11:08:57 +0100

    test

--- src/electrochemistry/e_implicit_exponential_mesh.hpp
+++ src/electrochemistry/e_implicit_exponential_mesh.hpp
@@ -1,15 +1,14 @@
 #ifndef E_IMPLICIT_EXPONENTIAL_MESH_HPP
 #define E_IMPLICIT_EXPONENTIAL_MESH_HPP
 
-#include <vector>
 #include <map>
 #include <string>
+#include <vector>
 
 #include "utilities.hpp"
 
-
 namespace pints {
-void e_implicit_exponential_mesh(map& params, vector& Itot, vector& t);
+void e_implicit_exponential_mesh(py::dict params, py::array Itot, py::array t);
 }
 
-#endif 
+#endif
