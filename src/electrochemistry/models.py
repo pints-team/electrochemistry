3b87545b72108702f7fc5383341d6e18418b70c0
Martin Robinson <martinjrobins@gmail.com>
2018-05-08 13:19:14 +0100

    update to newer pints

--- src/electrochemistry/models.py
+++ src/electrochemistry/models.py
@@ -348,8 +348,11 @@ class PintsModelAdaptor(pints.ForwardModel):
         self.ec_model = ec_model
         self.names = names
 
-    def dimension(self):
+    def n_parameters(self):
         return len(self.names)
+
+    def n_outputs(self):
+        return 1
 
     def simulate(self, parameters, times):
         self.ec_model.set_params_from_vector(parameters, self.names)
