90eee40bb8ec2191355fd6ea3455d00499bacc94
Martin Robinson <martinjrobins@gmail.com>
2018-07-24 14:13:26 +0100

    include python first

--- src/electrochemistry/e_implicit_exponential_mesh.hpp
+++ src/electrochemistry/e_implicit_exponential_mesh.hpp
@@ -1,11 +1,11 @@
 #ifndef E_IMPLICIT_EXPONENTIAL_MESH_HPP
 #define E_IMPLICIT_EXPONENTIAL_MESH_HPP
 
+#include "utilities.hpp"
+
 #include <map>
 #include <string>
 #include <vector>
-
-#include "utilities.hpp"
 
 namespace electrochemistry {
 void e_implicit_exponential_mesh(py::dict params, py::array_t<double> Itot,
